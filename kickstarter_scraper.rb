9c07d778f2772ba967e45d9797edf00b0d9661e9
Amber Taranto <ambzzzz@gmail.com>
2019-08-11 02:17:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,7 @@
 require 'nokogiri'
 
-def create_project_hash
-  # write your code here
-end
+    def create_project_hash
+        html = File.read('fixtures/kickstarter.html')
+
+        kickstarter = Nokogiri::HTML(html)
+    end
